4f2ec69f18acb6dcc869d3b954c38dcdf9f15f7c
VukoticMarko <50705538+VukoticMarko@users.noreply.github.com>
2022-05-23 13:43:13 +0200

    Added fresh project and integration

---
+++ Backend/drools-spring-app/yugioh_recommender/src/main/java/yugioh_recommend/YugiohRecommenderApplication.java
@@ -0,0 +1,43 @@
+package yugioh_recommend;
+
+import java.util.Arrays;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class YugiohRecommenderApplication {
+
+	private static Logger log = LoggerFactory.getLogger(YugiohRecommenderApplication.class);
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(YugiohRecommenderApplication.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+
+		StringBuilder sb = new StringBuilder("Application beans:\n");
+		for (String beanName : beanNames) {
+			sb.append(beanName + "\n");
+		}
+		log.info(sb.toString());
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
+	}
+
+}
---
+++ Backend/drools-spring-app/yugioh_recommender/src/test/java/yugioh_recommend/YugiohRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package yugioh_recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YugiohRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
